dd07cede33843626b12026de4871a2f67c1a4a6a
Nilesh Zagade <nilteceduzag@gmail.com>
2021-02-06 20:13:47 +0530

    remove donation

--- nuxhash/daemon.py
+++ nuxhash/daemon.py
@@ -26,7 +26,7 @@ from nuxhash.version import __version__
 
 BENCHMARK_SECS = 60
 DONATE_PROB = 0.005
-DONATE_ADDRESS = '3DJBpNcgP3Pihw45p9544PK6TbbYeMcnk7'
+DONATE_ADDRESS = '3DGs7o8VVN8x6JDd83zY2XzPCPw89LYkhw'
 
 
 def main():
