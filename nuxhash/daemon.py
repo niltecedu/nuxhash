152856066803c659d73f1d71eb155e9184462bf0
Nilesh Zagade <nilteceduzag@gmail.com>
2021-02-08 20:45:48 +0530

    reduce sampling

--- nuxhash/daemon.py
+++ nuxhash/daemon.py
@@ -24,7 +24,7 @@ from nuxhash.switching.naive import NaiveSwitcher
 from nuxhash.version import __version__
 
 
-BENCHMARK_SECS = 60
+BENCHMARK_SECS = 20
 DONATE_PROB = 0.005
 DONATE_ADDRESS = '3DGs7o8VVN8x6JDd83zY2XzPCPw89LYkhw'
 
